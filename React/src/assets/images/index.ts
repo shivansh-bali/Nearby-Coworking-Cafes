c528eadafb01d3eb643aa80a00a6148d85508bfd
shivansh-bali <bali.shivaansh@gmail.com>
2025-07-19 18:55:59 +0530

    Add files via upload

---
+++ React/src/assets/images/index.ts
@@ -0,0 +1,231 @@
+export { default as Logo } from "./logo.svg";
+export { default as googlePlay } from "./googlePlay.svg";
+export { default as appStore } from "./appStore.svg";
+export { default as favicon } from "./favicon.svg";
+export { default as slVector } from "./slvector.png";
+export { default as coffeeShop1 } from "./coffeeShop1.png";
+export { default as location } from "./location.svg";
+export { default as location2 } from "./location2.svg";
+export { default as logoNotchMobile } from "./logo-notch-mobile.svg";
+export { default as card1 } from "./card1.png";
+export { default as card2 } from "./card2.png";
+export { default as card3 } from "./card3.png";
+export { default as card4 } from "./card4.png";
+export { default as moon } from "./moon.svg";
+export { default as star } from "./star.svg";
+export { default as star2 } from "./star2.svg";
+export { default as coworking } from "./coworking.svg";
+export { default as LogoFooter } from "./logo-footer.svg";
+export { default as AnnaSmith } from "./annazSmith.svg";
+export { default as Gray } from "./BackgroundGray.svg";
+export { default as BlogImage } from "./BlogImage.png";
+export { default as romanImage } from "./romanImage.png";
+export { default as Yellow } from "./BackgroundYellow.svg";
+export { default as Boy } from "./userBoy.svg";
+export { default as Gril } from "./userGril.svg";
+export { default as ContactHeader } from "./HeaderImage.svg";
+export { default as Filled } from "./Filled.svg";
+export { default as PlaneIcon } from "./planeIcon.svg";
+export { default as previousIcon } from "./previousIcon.svg";
+export { default as nextIcon } from "./nextIcon.svg";
+export { default as Wifi } from "./Wifi.svg";
+export { default as Multiple } from "./Multiple.svg";
+export { default as Quite } from "./Quite.svg";
+export { default as BigTables } from "./BigTables.svg";
+export { default as Outside } from "./Outside.svg";
+export { default as Accessibility } from "./Accessibility.svg";
+export { default as Small } from "./Small.svg";
+export { default as Large } from "./Large.svg";
+export { default as FreeParking } from "./FreeParking.svg";
+export { default as Placeholder } from "./Placeholder.svg";
+export { default as filterIcon } from "./filterIcon.svg";
+export { default as reviewImg } from "./reviewImg.png";
+export { default as MapView2 } from "./MapView.svg";
+export { default as GridView } from "./GridView.svg";
+export { default as socket } from "./socket.svg";
+export { default as outside_dining } from "./outside_dining.svg";
+export { default as wifi} from "./Wifi.svg";
+export { default as large } from "./Large.svg";
+export { default as table } from "./table.svg";
+export { default as free } from "./free_parking.svg";
+export { default as time } from "./time.svg";
+export { default as number} from "./number.svg";
+export { default as webbrowser } from "./web_browsee.svg";
+export { default as menu } from "./menu.svg";
+export { default as review_icon } from "./review_icon.png";
+export { default as NadineLustre } from "./NadineLustre.svg";
+export { default as FaceBook } from "./facebookIcon.svg";
+export { default as reviewsHeader } from "./reviewsHeader.png";
+export { default as Photo1 } from "./Photo1.png";
+export { default as Photo2 } from "./Photo2.png";
+export { default as Photo3 } from "./Photo3.png";
+export { default as Photo4 } from "./Photo4.png";
+export { default as Photo5 } from "./Photo5.png";
+export { default as Photo6 } from "./Photo6.png";
+export { default as Photo7 } from "./Photo7.png";
+export { default as Photo8 } from "./Photo8.png";
+export { default as Photo9 } from "./Photo9.png";
+export { default as Photo10 } from "./Photo10.png";
+export { default as Photo11 } from "./Photo11.png";
+export { default as Photo12 } from "./Photo12.png";
+export { default as Photo13 } from "./Photo13.png";
+export { default as Photo14 } from "./Photo14.png";
+export { default as Photo15 } from "./Photo15.png";
+export { default as EditProfileIcon} from "./EditProfileIcon.svg";
+export { default as myprofilephone} from "./myprofilephone.png";
+export { default as profile_icon } from "./profile_icon.png";
+export { default as saved } from "./saved.svg";
+export { default as profile } from "./icon-profile.svg";
+export {default as dropDownIcon} from "./Dropdown.svg";
+export{ default as searchIcon} from "./SearchIcon.svg";
+export{ default as mapThumbnail} from "./Thumbnail.png";
+export{ default as upload} from "./upload.svg";
+export{ default as starIcon} from "./starIcon.svg";
+export{ default as mapImage} from "./mapImage.png";
+export { default as mapView } from "./MapView.svg";
+export {default as cafeImage} from "./CafeImage.png";
+export {default as transparentStar} from "./transparentStar.svg";
+export {default as userProfile} from "./userProfile.png";
+export {default as closeIcon} from "./closeIcon.svg";  
+export {default as PlayIcon} from "./PlayIcon.svg";
+export {default as PinSmall} from "./PinSmall.svg";
+export {default as InstaStory} from "./InstaStory.png";
+export {default as upload_icon} from "./upload_icon.png"
+export {default as TagCafe} from "./TagCafe.png"
+export {default as profileImg} from "./profileImg.svg"
+export {default as Banner1} from "./banner1.png"
+export {default as placeholderImage} from "./placeholderImage.png"
+
+
+export {default as right_icon} from "./right_icon.svg"
+export {default as blue_star} from "./blue_star.png"
+export {default as Yellow_shape} from "./yellow_shape.png"
+export {default as remote_work} from "./remote_work.svg"
+export {default as Union_shape1} from "./Union_shape1.png"
+export {default as Union_shape2} from "./Union_shape2.png"
+
+export {default as hand_shake} from "./hand_shake.svg"
+export {default as people} from "./people.svg"
+export {default as text_message} from "./text_message.svg"
+export {default as image_left} from "./image_left.png"
+export {default as image_right} from "./image_right.png"
+export {default as image_end} from "./image_end.png"
+export {default as join_community_bg} from "./join_community_bg.png"
+export {default as join_community} from "./join_community.png"
+export {default as cafe_view} from "./cafe_view.png"
+export {default as reelPlay} from "./reelPlay.svg"
+export {default as cafeImg} from "./cafeImg.jpg"
+
+export {default as headerBackground} from "./headerBackground.svg"
+export {default as Haley} from "./Haley.png"
+export {default as Carlos} from "./Carlos.png"
+export {default as mapIcon} from "./mapIcon.svg"
+export {default as filter}  from "./Filter.svg"
+
+export {default as dropIcon1} from "./dropIcon1.svg"
+export {default as dropIcon2} from "./dropIcon2.svg"
+export {default as dropIcon3} from "./dropIcon3.svg"
+export {default as dropIcon4} from "./dropIcon4.svg"
+export {default as dropIcon5} from "./dropIcon5.svg"
+export {default as dropIcon6} from "./dropIcon6.svg"
+export {default as dropIcon7} from "./dropIcon7.svg"
+
+export {default as logoWhite} from "./logoWhite.svg"
+
+export {default as cAcceptCards} from "./cAcceptCards.svg"
+export {default as cBigTables} from "./cBigTables.svg"
+export {default as cCashlessPayment} from "./cCashlessPayment.svg"
+export {default as cLargeGroups} from "./cLargeGroups.svg"
+export {default as cMultipleSockets} from "./cMultipleSockets.svg"
+export {default as cOutsideDining} from "./cOutsideDining.svg"
+export {default as cQuietVibe} from "./cQuietVibe.svg"
+export {default as cSmallGroups} from "./cSmallGroups.svg"
+export {default as cWifi} from "./cWifi.svg"
+export {default as cFreeParking} from "./cFreeParking.svg"
+
+export {default as cafeFormHeader} from "./cafeFormHeader.png"
+export {default as searchIconDark} from "./searchIconDark.svg"
+
+export {default as ourVision} from "./ourVision.svg"
+export {default as ourMission} from "./ourMission.svg"
+export {default as cafeViewYellow}  from "./cafeViewYellow.png"
+export {default as cafeViewGreen}  from "./cafeViewGreen.png"
+export {default as cafeViewPurple}  from "./cafeViewPurple.png"
+
+export {default as aboutNew}  from "./aboutNew.png"
+export {default as aboutPurple}  from "./aboutPurple.svg"
+export {default as aboutorange}  from "./aboutOrange.svg"
+export {default as aboutBlue}  from "./aboutBlue.svg"
+export {default as cafeStep}  from "./cafeStep.png"
+
+
+export {default as place1}  from "./place1.svg"
+export {default as place2}  from "./place2.svg"
+export {default as place3}  from "./place3.svg"
+export {default as place4}  from "./place4.svg"
+
+
+export {default as place5}  from "./place5.svg"
+export {default as place6}  from "./place6.svg"
+export {default as place7}  from "./place7.svg"
+export {default as place8}  from "./place8.svg"
+export {default as place9}  from "./place9.svg"
+export {default as place10}  from "./place10.svg"
+export {default as place11}  from "./place11.svg"
+export {default as place12}  from "./place12.svg"
+export {default as place13}  from "./place13.svg"
+export {default as place14}  from "./place14.svg"
+export {default as place15}  from "./place15.svg"
+export {default as place16}  from "./place16.svg"
+export {default as place17}  from "./place17.svg"
+export {default as place18}  from "./place18.svg"
+export {default as place19}  from "./place19.svg"
+export {default as place20}  from "./place20.svg"
+export {default as place21}  from "./place21.svg"
+export {default as place22}  from "./place22.svg"
+export {default as place23}  from "./place23.svg"
+
+export {default as uploadGallery}  from "./uploadGallery.svg"
+export {default as bannerimg}  from "./bannerimg.svg"
+
+export {default as iconS}  from "./iconS.svg"
+export {default as Intersect}  from "./Intersect.svg"
+export {default as eyeNew}  from "./eyeNew.svg"
+export {default as comingSoon}  from "./comingSoon.png"
+
+
+export {default as reBanner}  from "./reBanner.svg"
+export {default as timeIcon}  from "./timeIcon.svg"
+export {default as ownerImg}  from "./ownerImg.png"
+export {default as editNew}  from "./editNew.svg"
+export {default as addNew}  from "./addNew.svg"
+
+export {default as newsnacks}  from "./snacks.svg"
+export {default as star2NewHome}  from "./star2NewHome.svg"
+export {default as mark} from "./mark.svg"
+
+
+export {default as NSnack} from "./NSnack.svg"
+export {default as NBigTables} from "./NBigTables.svg"
+export {default as NOutside} from "./NOutside.svg"
+export {default as NPrivateRoom} from "./NPrivateRoom.svg"
+
+
+
+export {default as addImg}  from "./addImg.svg"
+export {default as addImg2}  from "./addImg2.svg"
+export {default as noDataFound}  from "./noDataFound.svg"
+
+
+export {default as signup}  from "./signup.png"
+export {default as signupPeople} from "./signupPeople.png"
+
+export {default as HomeFrame1} from './Home_Frame1.png'
+export {default as HomeFrame2} from './Home_Frame2.png'
+export {default as HomeFrame3} from './Home_Frame3.png'
+export {default as HomeFrame4} from './Home_Frame4.png'
+
+export {default as HomeWork1} from "./home-work-1.png"
+export {default as HomeWork2} from "./home-work-2.png"
+export {default as HomeWork3} from "./home-work-3.png"
+export {default as HomeWork4} from "./home-work-4.png"
