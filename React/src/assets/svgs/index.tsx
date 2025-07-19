0b5da1b1e3a1a1ccf83a03f22d9fb5cebbc3273e
shivansh-bali <bali.shivaansh@gmail.com>
2025-07-19 19:34:48 +0530

    Add files via upload

---
+++ React/src/assets/svgs/index.tsx
@@ -0,0 +1,53 @@
+export { default as Accessibility } from "./Accessibility.svg";
+export { default as BigTables } from "./BigTables.svg";
+export { default as Books } from "./Books.svg";
+export { default as ChildSeats } from "./ChildSeats.svg";
+export { default as FastWifi } from "./FastWifi.svg";
+export { default as HighTops } from "./HighTops.svg";
+export { default as Keys } from "./Keys.svg";
+export { default as LargeGroup } from "./LargeGroup.svg";
+export { default as Meals } from "./Meals.svg";
+export { default as MultipleSockets } from "./MultipleSockets.svg";
+export { default as NaturalLight } from "./NaturalLight.svg";
+export { default as Outdoor } from "./Outdoor.svg";
+export { default as Parking } from "./Parking.svg";
+export { default as Printer } from "./Printer.svg";
+export { default as QuietVibes } from "./QuietVibes.svg";
+export { default as SmallGroups } from "./SmallGroups.svg";
+export { default as Snacks } from "./Snacks.svg";
+export { default as SofaSeating } from "./SofaSeating.svg";
+export { default as WiFi } from "./Wifi.svg";
+export { default as Alcohol } from "./Alchohol.svg";
+export { default as Barseating } from "./Bar-seating.svg";
+export { default as Handicap } from "./Handicap.svg";
+export { default as HighChairs } from "./HighChairs.svg";
+export { default as OpenNow } from "./OpenNow.svg";
+export { default as Outlets } from "./Outlets.svg";
+export { default as PriceRange } from "./PriceRange.svg";
+export { default as SomethingToEat } from "./SomethingToEat.svg";
+export { default as Stars } from "./Stars.svg";
+export { default as Students } from "./Students.svg";
+export { default as TableLayout } from "./TableLayout.svg";
+
+export { default as Location } from "./Location.svg";
+export { default as Location2 } from "./Location2.svg";
+
+export { default as BarPin } from "./barPin.svg";
+export { default as CafePin } from "./cafePin.svg";
+export { default as CoworkingPin } from "./coworkingPin.svg";
+export { default as RestroPin } from "./restroPin.svg";
+export { default as mapIcon } from "./mapIcon.svg";
+
+export {default as Bar} from './Bar.svg'
+export {default as Brewery} from './Brewery.svg';
+export {default as Book} from './Book.svg';
+export {default as Gym} from './Gym.svg';
+export {default as Hotel} from './Hotel.svg';
+export {default as Museum} from './Museum.svg';
+export {default as CoWorkingSpace} from './Co-working Space.svg';
+export {default as Restaurants} from './Restaurants.svg';
+export {default as CoffeeShopIcon} from './Coffee Shop.svg';
+export {default as PaperPlane} from "./PaperPlane.svg";
+export {default as AppleStore} from "./Apple Store.svg";
+export {default as GoogleStore} from "./Google Store.svg";
+
